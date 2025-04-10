b80ec39dce0f3f80ecde545ab924724be06327e2
twinsuns27 <joaotolomelli@gmail.com>
2025-04-10 17:11:34 -0300

    feat: tailwind support

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logoUnifor from "./assets/logo-unifor.svg"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-blue-800 rounded-b-xl px-[4rem] py-[1rem] flex flex-row justify-between">
+        <div>
+          <img className="w-[2.5rem]" src={logoUnifor} alt="Logo Unifor" />
+        </div>
+        <h1>profile</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
